79d38301c3ea622fcd2e0492e12f997d4f616077
Pablo Wendrey <pabllowendrey@gmail.com>
2022-08-09 18:18:52 -0300

    First Model-Controller-View - Department

--- SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -1,7 +1,10 @@
 using System;
 
-namespace SalesWebMvc.Models {
-    public class ErrorViewModel {
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
