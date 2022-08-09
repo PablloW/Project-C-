ddb8cdbe4b948e4efe22682161d01412cfda87b5
Pablo Wendrey <pabllowendrey@gmail.com>
2022-08-09 17:00:26 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -13,7 +13,9 @@ namespace SalesWebMvc.Controllers {
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C#.";
+            ViewData["Developer"] = "Pablo Wendrey";
+            ViewData["Email"] = "pabllowendrey@gmail.com";
 
             return View();
         }
